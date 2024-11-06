18fe60b3738f52c0819ff75834282ed9c22667d9
chelsiaa17 <chelsiana17@gmail.com>
2024-11-06 20:08:33 +0000

    I made the website responsive for mobile viewing

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       e.preventDefault();
       
       const targetElement = document.querySelector(this.getAttribute("href"));
-      
-      // Calculate the position 10vh (10% of viewport height) above the target element
-      const offsetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - (window.innerHeight * 0.1);
+      const offsetPosition = targetElement.getBoundingClientRect().top + window.scrollY - (window.innerHeight * 0.1);
       
       window.scrollTo({
-        top: offsetPosition,
-        behavior: "smooth"
+        top: offsetPosition
       });
     });
   });
